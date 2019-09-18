f5652a7f369eff2386242136e85a1b15ecf11f37
Darshak Rairakhia <42878805+Darshak1997@users.noreply.github.com>
2019-09-18 15:45:58 -0500

    Update 8.1.py

--- 8.1.py
+++ 8.1.py
@@ -4,6 +4,7 @@ Created on Wed Sep 18 15:04:14 2019
 
 @author: Darshak
 """
+# Brute Force
 n = input("Enter Steps: ")
 def countways(n):
     
@@ -14,4 +15,16 @@ def countways(n):
     else:
         return countways(n-1)+countways(n-2)+countways(n-3)
     
+print(countways(n))
+
+# Memoization
+def countways(n):
+    mem = [0]*(n+1)
+    mem[0] = 1
+    mem[1] = 1
+    mem[2] = 2
+    for i in range(3, n+1):
+        mem[i] = mem[i-1]+mem[i-2]+mem[i-3]
+    return mem[n]
+
 print(countways(n))
